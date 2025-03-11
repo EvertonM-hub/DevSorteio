a16b53155391e941e12468c6005237c81c9d04cb
EvertonM-hub <evertonmoreira19@hotmail.com>
2025-03-11 14:38:12 -0300

    numeros min e max

--- arquivo.js
+++ arquivo.js
@@ -4,9 +4,14 @@ function generateNumber(){
      const min = Math.ceil (document.querySelector(".input-min").value)
      const max = Math.floor (document.querySelector(".input-max").value)
 
+     if(min>=max){
+      alert("Vamor minimo tem que ser MENOR que o vamor MÀXIMO")
+     } else{
+
      const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
           alert(result)
+        }
       }
 
 
